1af70bc854d7e4ed979cfb55dc0f33735a5206a8
Gabriel Bravo <>
2021-01-12 19:11:43 -0400

    first commit

---
+++ src/test/java/com/bravo/onlinestoreapi/OnlineStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravo.onlinestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
