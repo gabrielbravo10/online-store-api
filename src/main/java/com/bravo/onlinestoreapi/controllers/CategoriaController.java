fa16ee45c6d7b4129f536043467761841276bca6
Gabriel Bravo <gabriel.bravo@totemti.com.br>
2021-01-14 10:41:22 -0400

    rest test

---
+++ src/main/java/com/bravo/onlinestoreapi/controllers/CategoriaController.java
@@ -0,0 +1,29 @@
+package com.bravo.onlinestoreapi.controllers;
+
+import com.bravo.onlinestoreapi.entities.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "categorias")
+public class CategoriaController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+
+    }
+
+}
