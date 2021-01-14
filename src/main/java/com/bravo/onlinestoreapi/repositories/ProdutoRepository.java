991055f9a3b3c3d71eb0d535bc03e45b40dbac10
Gabriel Bravo <gabriel.bravo@totemti.com.br>
2021-01-14 12:38:42 -0400

    produto, categoria ManyToMany

---
+++ src/main/java/com/bravo/onlinestoreapi/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.bravo.onlinestoreapi.repositories;
+
+import com.bravo.onlinestoreapi.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
